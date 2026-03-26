0fc509586742a954e9e888c4761db751ba051621
BhowmickSouvik <bhowmick.souvik2701@gmail.com>
2026-03-26 09:56:43 +0530

    add geneter_qr

---
+++ jarvis.py
@@ -0,0 +1,51 @@
+import speech_recognition as sr
+import webbrowser
+import pyttsx3
+import musiclibrary
+
+def speak(text):
+    engine = pyttsx3.init()
+    engine.say(text)
+    engine.runAndWait()
+
+def processcommand(c):
+    if "open google" in c.lower():
+        webbrowser.open("https://google.com")
+    elif "open youtube" in c.lower():
+        webbrowser.open("https://youtube.com")
+
+    elif c.lower().startswith("play"):
+        song = c.lower().split(" ")[1]
+        link = musiclibrary.music[song]
+        webbrowser.open(link)
+
+
+if __name__ == "__main__":
+    speak("initializing jarvis")
+
+
+while True:
+    recog = sr.Recognizer()
+
+    print("recognizing")
+    try:
+        with sr.Microphone() as source:
+            recog.adjust_for_ambient_noise(source)
+            print("Lestening...")
+            audio = recog.listen(source, timeout=1.8, phrase_time_limit=1)
+        word = recog.recognize_google(audio)
+
+        if(word.lower() == "jarvis"):
+            speak("yes sir")
+
+            with sr.Microphone() as source:
+                recog.adjust_for_ambient_noise(source)
+                print("Jarvis Active...listenning for command")
+                audio = recog.listen(source)
+                recog.pause_threshold=0.7
+            command = recog.recognize_google(audio)
+            processcommand(command)
+
+
+    except Exception as e:
+        print("Error; {0}".format(e))
---
+++ musiclibrary.py
@@ -0,0 +1,4 @@
+music = {
+    "birds" : "https://youtu.be/vOXZkm9p_zY?si=zeHJtEqcOrZzs8xm",
+    "natural":"https://youtu.be/0I647GU3Jsc?si=g28duWABLG906s1Z"
+}
